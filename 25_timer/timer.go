841a646a204db31a5c08c748ec950c3c6dbcc3b6
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-08-04 12:08:02 +0700

    until tugas 10

---
+++ 25_timer/timer.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Mulai")
+	time.Sleep(time.Second * 3)
+	fmt.Println("Selesai setelah 3 detik")
+}
---
+++ Tugas10/tugas10.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello, namaku Khalil")
+	time.Sleep(time.Second * 5)
+	fmt.Println("Hobi saya Main game")
+	time.Sleep(time.Second * 5)
+	fmt.Println("Tanggal lahir saya 09 Agustus 2000")
+}
